63528aab7686b552fee12111fa60119c3703ec1d
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2019-11-09 08:52:08 +0100

    pushing images to docker hub

--- mysql/build.sh
+++ mysql/build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 if [ -z $1 ]; then
+  echo "ERROR: No docker hub user provided."
+  echo " Syntax: "$0" username vX.YY"
+  exit 2
+fi
+
+if [ -z $2 ]; then
   echo "ERROR: No version provided."
-  echo " Syntax: "$0" vX.YY"
+  echo " Syntax: "$0 $1" vX.YY"
   exit 2
 fi
 
 sudo rm -rf mysql_data
-ID=$(docker build -q -t cns_mysql:$1 .)
-docker tag $ID angelalonso/cns_mysql:$1
-docker push angelalonso/cns_mysql:$1
+ID=$(docker build -q -t cns_mysql:$2 .)
+docker tag $ID $1/cns_mysql:$2
+docker push $1/cns_mysql:$2
